3fbcc6815d05dae1bb3d176273ad030dedfbb20c
Hoku113 <83630260+Hoku113@users.noreply.github.com>
2021-09-13 22:40:51 +0900

    Revert "Test"

--- src/main.js
+++ src/main.js
@@ -1,19 +1,8 @@
-
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App'
-import {routes} from './routes'
+import App from './App.vue'
 
-
-Vue.use(VueRouter)
-const router = new VueRouter({
-  mode:'history',
-  routes
-})
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
-});
+  render: h => h(App),
+}).$mount('#app')
